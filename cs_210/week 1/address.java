873f151a0e0a66102cdff7dcd0284bd676cf4343
axmadjon63 <250221@newuu.uz>
2026-03-10 12:45:06 +0500

    Delete cs_210/week 1 directory

--- cs_210/week 1/address.java
+++
@@ -1,5 +0,0 @@
-public class address {
-    public static void main(String[] args) {
-        System.out.println("Axmadjon Tolibjonov \nMovarounnahr 1 \nTashkent 10093 \nUzbekistan");
-    }
-}
--- cs_210/week 1/area.java
+++
@@ -1,9 +0,0 @@
-public class area {
-    public static void main(String[] args){
-        Double width = 4.9;
-        Double height = 7.5;
-        Double area = width * height;
-        Double peri = 2*(width + height);
-        System.out.printf("Area: %.2f, Perimeter: %.2f", area, peri);
-    }
-}
